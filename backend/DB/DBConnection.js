098967053e22bf0d4a24189f8bae02ddb4c5336c
Mahmoud Muhammad Kamal <mahmoudk63@gmail.com>
2020-09-30 19:51:21 +0200

    env file

--- backend/DB/DBConnection.js
+++ backend/DB/DBConnection.js
@@ -1,10 +1,10 @@
-const { host, user, password, database } = require('../config');
+const { host, user, password, database } = require("../config");
 
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize('vw', 'root', 'admin', {
-  dialect: 'mysql',
-  host: 'localhost',
+const sequelize = new Sequelize("vw", "app", "KIMO!@#321kimo", {
+  dialect: "mysql",
+  host: "localhost",
 });
 
 module.exports = sequelize;
